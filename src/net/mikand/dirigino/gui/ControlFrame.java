f100e4a8abf20ef31d492923ce57b6cd4e0eb493
mikand <micheli.andrea@gmail.com>
2013-09-14 12:48:29 +0200

    Fixed warning

--- src/net/mikand/dirigino/gui/ControlFrame.java
+++ src/net/mikand/dirigino/gui/ControlFrame.java
@@ -10,10 +10,8 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-import net.mikand.dirigino.control.SerialInterface;
 import net.mikand.dirigino.utils.MathUtils;
 
 public class ControlFrame extends JFrame {
